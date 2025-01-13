af9d7253dbf0fa9bf65ad60359e94f9eb04d1970
Oliver Gregory <oliver@Olivers-Laptop.local>
2025-01-13 22:49:52 +0000

    add candlestick

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import hashlib
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
+import mplfinance as mpf
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
@@ -423,6 +424,67 @@ def get_top_performing_stocks(user):
     df = conn.query(query)
     
     return df
+
+def get_candlestick_chart(ticker):
+    
+    collection = database[ticker] # Access the relevant collection in the MongoDB database
+    
+    data = collection.find().sort('_id', -1)
+    
+    df = pd.DataFrame(list(data))
+    
+    df = df[['start_time', 'open_price', 'close_price', 'min_price', 'max_price']].rename(columns={'start_time': 'Datetime', 'open_price': 'Open', 'close_price': 'Close', 'min_price': 'Low', 'max_price': 'High'}, inplace=False)
+    
+    df['Datetime'] = pd.to_datetime(df['Datetime'])
+    df.set_index('Datetime', inplace=True)
+    df.sort_index(inplace=True)
+    
+    mc = mpf.make_marketcolors(
+        up='green',
+        down='red',
+        edge={'up': 'green', 'down': 'red'},
+        volume='in',
+        inherit=True
+    )
+    
+    s = mpf.make_mpf_style(
+        marketcolors=mc,
+        gridcolor='w',
+        gridstyle='--',
+        facecolor='none',
+        edgecolor='w',
+        figcolor='none',
+        y_on_right=False,
+        rc={
+               'text.color': 'w',
+               'axes.labelcolor': 'w',
+               'axes.titlecolor': 'w',
+               'xtick.color': 'w',
+               'ytick.color': 'w'
+           }
+    )
+    
+    fig, ax = mpf.plot(
+        df,
+        type='candle',
+        style=s,
+        ylabel_lower='Volume',
+        datetime_format='%H:%M',
+        returnfig=True
+    )
+    
+    fig.patch.set_alpha(0)
+    ax[0].patch.set_alpha(0)
+    ax[0].set_ylabel(None)
+    
+    ax[0].spines["top"].set_visible(False)
+    ax[0].spines["right"].set_visible(False)
+    ax[0].spines["left"].set_visible(True)
+    ax[0].spines["bottom"].set_visible(True)
+    
+    ax[0].grid(alpha=0.3)
+    
+    return fig
 
 def manager_view(user):
 
